73dc29ae75e96064d21d1d83d5d1e811366b793d
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2023-07-13 13:49:39 +0300

    added the ability to process cells
    added the ability to process cells. Players can see different colors of cells "x" and "o".

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -22,16 +22,33 @@ white = (255,255,255)
 green = (0,255,0)
 
 mas = [[0] * 3 for i in range(3)]
+query = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (margin + size_block)
+            row = y_mouse // (margin + size_block)
+            if mas[row][col] == 0:
+                if query%2 == 0:
+                    mas[row][col] = 'x'
+                else:
+                    mas[row][col] = 'o'
+                query+=1
 # задаем координаты клеток
     for row in range(3):
         for col in range(3):
+            if mas[row][col] == 'x':
+                color = red
+            elif mas[row][col] == 'o':
+                color = green
+            else:
+                color = white
             x = col * size_block + (col + 1) * margin
             y = row * size_block + (row + 1) * margin
-            pygame.draw.rect(screen,white,(x,y,size_block,size_block))
+            pygame.draw.rect(screen,color,(x,y,size_block,size_block))
     pygame.display.update()
